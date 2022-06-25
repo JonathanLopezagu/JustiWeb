f4930b014a1795a535765e753b3f4a6180ee5f0c
Jonathan Lopez <jon.14lam@gmail.com>
2022-06-25 00:03:33 -0500

    Seccion Deportes completada

--- archivosJS/barraNavegacion.js
+++ archivosJS/barraNavegacion.js
@@ -6,3 +6,11 @@ toggleNav.addEventListener("click", ()=>
    
     navBar.classList.toggle("navBar-visible");
 });
+
+/*
+let espacio="";
+let holamundo= "hola" + espacio +"mundo";
+
+console.log(holamundo);
+
+*/
