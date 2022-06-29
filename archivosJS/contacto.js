a23ccbf71e39ff922012848e4deae08c14f8d2ec
Jonathan Lopez <jon.14lam@gmail.com>
2022-06-28 23:48:30 -0500

    Formulario terminado

---
+++ archivosJS/contacto.js
@@ -0,0 +1,23 @@
+let btnEnviar = document.querySelector('.botonEnviar');
+
+btnEnviar.addEventListener('click', (e) => {
+    e.preventDefault();
+    swal("Enviado", "Datos enviados, nos comunicaremos contigo", "success");
+
+});
+
+let btnEnviar2 = document.querySelector('.botonEnviar2');
+
+btnEnviar2.addEventListener('click', (e) => {
+    e.preventDefault();
+    swal("Enviado", "Datos enviados, nos comunicaremos contigo", "success");
+
+});
+
+let btnEnviar3 = document.querySelector('.botonEnviar3');
+
+btnEnviar3.addEventListener('click', (e) => {
+    e.preventDefault();
+    swal("Enviado", "Datos enviados, nos comunicaremos contigo", "success");
+
+});
